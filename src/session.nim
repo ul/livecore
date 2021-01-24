43679e9a8baa706c233a4f155f272918fd7f6875
Ruslan Prakapchuk <fer.obbee@gmail.com>
2021-01-24 12:29:23 +1100

    Sun 24 Jan 2021 12:29:23 AEDT

--- src/session.nim
+++ src/session.nim
@@ -8,11 +8,38 @@ import
 type
   State* = object
     pool: Pool
-    phase1: float
 
 proc process*(s: var State): Frame {.nimcall, exportc, dynlib.} =
   s.pool.init
-  0.1 * (220.0.osc + 220.0.osc(s.phase1))
+  let
+    f = 120.rline.to(33, 45).quantize(2).add(1).midi2freq
+    drone =
+      ((f.tri*f.saw).bqlpf(2.0*f, (1/8).osc.to(0.5, 1.5)) *
+       60.rline.to(12, 16).round.dmetro.impulse(2))
+      .fb(4, 0.6)
+    cm = 7/((1/16).osc.to(3, 7).round)
+    m = (1/8).osc.to(1/4, 2).dmetro
+    base = 8.rline.to(57, 69).round
+    e = m.impulse(0.05)
+    Q = 2.0.sqrt
+    chirp = [
+        [base, base+24].sequence(m),
+        base+[2.0, 3.0, 4.0].sequence(m),
+        base+[3.0, 5.0, 7.0].sequence(m),
+        base+12,
+      ].sequence(m).midi2freq.fm(cm, 1/2).mul(e)
+      .bqnotch(4.osc.to(base, base+24).midi2freq, Q)
+      .fb((1/32).rline.to(1/24, 4), 0.25)
+      .fb(1/16, 0.2)
+      .fb(1/8, 0.2)
+      .fb(1/7, 0.2)
+      .fb(1/5, 0.2)
+      .fb(1/3, 0.2)
+      .bqlpf((1/32).osc.to(69, 93).round.midi2freq, Q)
+    mix =
+      0.25 * drone.pan(0.5 * (1/12).osc) +
+      0.4  * chirp.pan(0.5 * (1/8).osc)
+  mix.bqhpf(30.0, Q).mul(0.5).saturator
 
 # A place for heavy init logic, like reading tables from the disk.
 # Beware access to the state is not guarded and may happen simultaneously with `process`.
