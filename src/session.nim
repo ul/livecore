154c2c0a7753a55537e0211343acd34362cca27b
Ruslan Prakapchuk <fer.obbee@gmail.com>
2021-01-30 10:01:32 +1100

    Sat 30 Jan 2021 10:01:32 AEDT

--- src/session.nim
+++ src/session.nim
@@ -47,12 +47,13 @@ proc process*(s: var State): Frame {.nimcall, exportc, dynlib.} =
       bt(20.0)
       .maygate(0.5)
       .adsr(0.05, 0.2, 0.6, 0.5)
+    e = bt(40.0).maytrig(0.5).gaussian(0.05, 55.osc.biscale(0.05, 0.1))
     t2 = [69.0, 81.0, 93].choose(bt(30.0))
       .tline(0.05)
       .sub([12.0, 24.0, 36.0].choose(bt(30.0)))
       .midi2freq
       .bltriangle
-      .mul(bt(40.0).maytrig(0.5).gaussian(0.05, 55.osc.biscale(0.05, 0.1)))
+      .mul(e)
       .conv([white_noise().lpf(1.0)*0.1, white_noise().lpf(1.0)*0.2, 0.99], s.cnv)
       .fb((1/12).tri.biscale(1/11, 1/10), 0.5)
       .long_fb(20.0, 0.7071)
