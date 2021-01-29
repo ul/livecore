e8a2e22e5ee4e4ac2b8637767bcf95b551b1b9cc
Ruslan Prakapchuk <fer.obbee@gmail.com>
2021-01-30 08:28:31 +1100

    Sat 30 Jan 2021 08:28:31 AEDT

--- src/session.nim
+++ src/session.nim
@@ -20,7 +20,7 @@ proc process*(s: var State): Frame {.nimcall, exportc, dynlib.} =
       .midi2freq
       .fm(7/3, 3/4) *
       bt(20.0)
-      .maygate(white_noise().scale(1/4, 1/2))
+      .maygate(0.5)
       .adsr(0.1, 0.1, 0.6, 0.25)
     t2 = [69.0, 81.0, 93]
       .sequence(bt(60.0))
