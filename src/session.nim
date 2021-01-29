ca1cad9ba819c73d60c5502bbc37e72bb9b06b9d
Ruslan Prakapchuk <fer.obbee@gmail.com>
2021-01-30 08:22:57 +1100

    Sat 30 Jan 2021 08:22:57 AEDT

--- src/session.nim
+++ src/session.nim
@@ -30,7 +30,7 @@ proc process*(s: var State): Frame {.nimcall, exportc, dynlib.} =
       .mul(bt(80.0).impulse(0.01))
       .long_fb(20, 0.7071)
     mix = t1.zitarev(level=0.3) + 0.1*t2
-  mix.phaser.simple_saturator
+  mix.compressor(200.0, -12.0, 0.1, 0.1).simple_saturator
 
 # A place for heavy init logic, like reading tables from the disk.
 # Beware access to the state is not guarded and may happen simultaneously with `process`.
