46c64b4c7ed4cc5fa72ec5260bded65c6ca69b78
Ruslan Prakapchuk <fer.obbee@gmail.com>
2021-01-30 10:06:43 +1100

    Sat 30 Jan 2021 10:06:43 AEDT

--- src/session.nim
+++ src/session.nim
@@ -60,7 +60,7 @@ proc process*(s: var State): Frame {.nimcall, exportc, dynlib.} =
       .zitarev(level=0)
       .long_fb(30.0, 0.5)
       .wpkorg35(10000.0, 1.0, 0.0)
-      .bqhpf(1.osc.biscale(@30, @60), 0.7071)
+      .bqhpf(11.osc.biscale(@30, @60), 0.7071)
     mix = 0.0*t1.zitarev(level=0) + 0.3*t2
   mix.bqhpf(30.0, 0.7071).compressor(200.0, -12.0, 0.1, 0.1).simple_saturator
 
