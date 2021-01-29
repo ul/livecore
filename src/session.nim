ba0397011c97563b4242cf3361772c0930ee8a93
Ruslan Prakapchuk <fer.obbee@gmail.com>
2021-01-30 10:22:53 +1100

    Sat 30 Jan 2021 10:22:53 AEDT

--- src/session.nim
+++ src/session.nim
@@ -38,7 +38,7 @@ type
 
 proc process*(s: var State): Frame {.nimcall, exportc, dynlib.} =
   s.pool.init
-  let clk = [2.0, 3.0].choose(20.dmetro).bpm2freq.saw
+  let clk = 2.0.bpm2freq.saw
   template bt(n: float): float = clk.phsclk(n)
   let
     e = bt(40.0).maytrig(0.5).gaussian(0.05, 55.osc.biscale(0.05, 0.1))
