4ad611c474834cf242c1e502d99e7642317d8e55
Ruslan Prakapchuk <fer.obbee@gmail.com>
2021-01-30 08:41:38 +1100

    Sat 30 Jan 2021 08:41:38 AEDT

--- src/session.nim
+++ src/session.nim
@@ -29,6 +29,7 @@ proc process*(s: var State): Frame {.nimcall, exportc, dynlib.} =
       .mul(bt(80.0).impulse(0.01))
       .pshift((1/60).osc.mul(2.0), 256, 64)
       .long_fb(20, 0.7071)
+      .wpkorg35(@93, 1.0, 0.0)
     mix = t1.zitarev(level=0.3) + 0.3*t2
   mix.compressor(200.0, -12.0, 0.1, 0.1).simple_saturator
 
