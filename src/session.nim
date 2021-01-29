41816a6e68d3e5d0e25b4f6afe8877a079d439a3
Ruslan Prakapchuk <fer.obbee@gmail.com>
2021-01-30 09:18:20 +1100

    Sat 30 Jan 2021 09:18:20 AEDT

--- src/session.nim
+++ src/session.nim
@@ -19,6 +19,10 @@ proc maytrig(t, p: float): float =
   return 0.0
 lift2(maytrig)
 
+proc decim(x, p: float): float =
+  if white_noise() < p: x else: 0.0
+lift2(decim)
+
 proc process*(s: var State): Frame {.nimcall, exportc, dynlib.} =
   s.pool.init
   let clk = [0.5, 1.0, 2.0].choose(20.dmetro).bpm2freq.saw
@@ -39,6 +43,7 @@ proc process*(s: var State): Frame {.nimcall, exportc, dynlib.} =
       .fb((1/12).tri.biscale(1/11, 1/10), 0.5)
       .long_fb(20, 0.7071)
       .wpkorg35(5.osc.biscale(@54, @69), 2.osc.biscale(0.5, 1.0), 0.0)
+      .decim(0.99)
     mix = 0.0*t1.zitarev(level= -10) + 0.3*t2
   mix.bqhpf(30.0, 0.7071).compressor(200.0, -12.0, 0.1, 0.1).simple_saturator
 
