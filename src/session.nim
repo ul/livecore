c0893953cf5ea36e1a06c525ad4ea53fd71e05a9
Ruslan Prakapchuk <fer.obbee@gmail.com>
2021-01-30 07:48:29 +1100

    Sat 30 Jan 2021 07:48:29 AEDT

--- src/session.nim
+++ src/session.nim
@@ -12,7 +12,7 @@ type
 proc process*(s: var State): Frame {.nimcall, exportc, dynlib.} =
   s.pool.init
   let
-    clk = 60.bpm2freq.saw
+    clk = 30.bpm2freq.saw
     t1 = 220.osc * clk.phsclk(2.0).adsr(0.1, 0.01, 0.8, 0.05)
     mix = t1.zitarev
   mix.simple_saturator
