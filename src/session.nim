d3e74116e61c73d4f6cd17d7db04e4df3a93df63
Ruslan Prakapchuk <fer.obbee@gmail.com>
2021-01-30 08:44:22 +1100

    Sat 30 Jan 2021 08:44:22 AEDT

--- src/session.nim
+++ src/session.nim
@@ -14,8 +14,9 @@ proc process*(s: var State): Frame {.nimcall, exportc, dynlib.} =
   let clk = (1/10).bpm2freq.osc.biscale(1, 1/2).bpm2freq.saw
   template bt(n: float): float = clk.phsclk(n)
   let
-    t1 = [45.0, 48, 51][white_noise().sh(bt(30.0)).mul(3).int]
-      .tline(0.05)
+    # t1 = [45.0, 48, 51][white_noise().sh(bt(30.0)).mul(3).int]
+    #   .tline(0.05)
+    t1 = white_noise().sh(bt(30.0)).tline(1.0).scale(45.0, 51.0)
       .midi2freq
       .fm(7/3, 3/4) *
       bt(20.0)
