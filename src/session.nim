5f503ea38f974b33581c1c756277df65ddc6f8f6
Ruslan Prakapchuk <fer.obbee@gmail.com>
2021-01-30 10:44:27 +1100

    Sat 30 Jan 2021 10:44:27 AEDT

--- src/session.nim
+++ src/session.nim
@@ -51,7 +51,7 @@ proc process*(s: var State): Frame {.nimcall, exportc, dynlib.} =
       # .blsquare((1.30).osc.biscale(0.01, 0.5)).bi
       .bltriangle.bi
       # .osc
-      .mul(f.osc)
+      .circle.sin
       .mul(e)
       .pan((1/60).osc.mul(1/4))
       .conv([white_noise().lpf(1/20)*0.1, white_noise().lpf(1/20)*0.2, 0.99], s.cnv)
