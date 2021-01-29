12ac08a5b35ff83ced1d24dec8fecb5091227599
Ruslan Prakapchuk <fer.obbee@gmail.com>
2021-01-30 09:02:47 +1100

    Sat 30 Jan 2021 09:02:47 AEDT

--- src/session.nim
+++ src/session.nim
@@ -26,7 +26,7 @@ proc process*(s: var State): Frame {.nimcall, exportc, dynlib.} =
       .bltriangle
       .mul(bt(40.0).gaussian(0.1, 0.1))
       .long_fb(20, 0.7071)
-      .fb((1/12).tri.biscale(1/11, 1/10), 0.2)
+      .fb((1/12).tri.biscale(1/11, 1/10), 0.5)
       .wpkorg35(@93, 1.0, 0.0)
     mix = 0.0*t1.zitarev(level= -10) + 0.3*t2
   mix.bqhpf(30.0, 0.7071).compressor(200.0, -12.0, 0.1, 0.1).simple_saturator
