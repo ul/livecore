897aefd42190348177a5797e2ebc30bdf4dfaa01
Ruslan Prakapchuk <fer.obbee@gmail.com>
2021-01-30 11:23:22 +1100

    Sat 30 Jan 2021 11:23:22 AEDT

--- src/session.nim
+++ src/session.nim
@@ -80,7 +80,7 @@ proc process*(s: var State): Frame {.nimcall, exportc, dynlib.} =
       # .long_fb(30.0, 0.5)
       .wpkorg35(10000.0, 1.0, 0.0)
       .conv([white_noise().lpf(1/20)*0.1, white_noise().lpf(1/20)*0.2, 0.7], s.cnv2)
-    mix = 0.3* 220.osc
+    mix = 0.3*t2
   mix.bqhpf(30.0, 0.7071).compressor(200.0, -12.0, 0.1, 0.1).simple_saturator
 
 # A place for heavy init logic, like reading tables from the disk.
