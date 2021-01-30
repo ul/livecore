f7478c352198dd47b9a503af5334a8f6a1b13965
Ruslan Prakapchuk <fer.obbee@gmail.com>
2021-01-30 14:48:15 +1100

    choose, conv, decim, maytrig

--- src/pool.nim
+++ src/pool.nim
@@ -3,41 +3,44 @@
 
 import
   dsp/[
-    frame, delays, envelopes, events, filters, metro, modules, noise, osc, soundpipe
+    frame, delays, effects, envelopes, events, filters, metro, modules, noise, osc, soundpipe
   ]
 
 const
-  medium_pool = 0x40
+  large_pool = 0x100
+  medium_pool = 0x80
   small_pool = 0x10
 
 type
   Data = object
     adsr: array[medium_pool, ADSR]
-    autowah: array[small_pool, AutoWah]
-    bigverb: array[small_pool, BigVerb]
+    autowah: array[medium_pool, AutoWah]
+    bigverb: array[medium_pool, BigVerb]
     biquad: array[medium_pool, BiQuad]
-    bitcrush: array[small_pool, BitCrush]
+    bitcrush: array[medium_pool, BitCrush]
     blsaw: array[medium_pool, BlSaw]
     blsquare: array[medium_pool, BlSquare]
     bltriangle: array[medium_pool, BlTriangle]
     brown_noise: array[medium_pool, BrownNoise]
     chaos_noise: array[medium_pool, ChaosNoise]
-    clock: array[small_pool, Clock]
-    compressor: array[small_pool, Compressor]
+    choose: array[medium_pool, Choose]
+    clock: array[medium_pool, Clock]
+    compressor: array[medium_pool, Compressor]
+    conv: array[medium_pool, Conv]
     delay: array[medium_pool, Delay[1.seconds]]
     diode: array[medium_pool, Diode]
     hpf: array[medium_pool, HPF]
     fm: array[medium_pool, FM]
-    jcrev: array[small_pool, JCRev]
+    jcrev: array[medium_pool, JCRev]
     long_delay: array[small_pool, Delay[30.seconds]]
     maygate: array[medium_pool, MayGate]
     metro: array[medium_pool, Metro]
     peaklim: array[medium_pool, PeakLimiter]
-    phaser: array[small_pool, Phaser]
+    phaser: array[medium_pool, Phaser]
     pink_noise: array[medium_pool, PinkNoise]
     pshift: array[medium_pool, PShift]
     rline: array[medium_pool, RLine]
-    sample: array[medium_pool, float]
+    sample: array[large_pool, float]
     saturator: array[medium_pool, Saturator]
     sequence: array[medium_pool, int]
     transition: array[medium_pool, Transition]
@@ -55,8 +58,10 @@ type
       bltriangle,
       brown_noise,
       chaos_noise,
+      choose,
       clock,
       compressor,
+      conv,
       delay,
       diode,
       hpf,
@@ -161,6 +166,7 @@ def3(diode)
 def3(fm)
 def3(gaussian, sample)
 def4(autowah)
+def4(conv)
 def4(peaklim)
 def4(pshift)
 def4(wpkorg35)
@@ -198,6 +204,14 @@ proc sequence*(seq: openArray[float], t: float): float =
 proc sequence*(seq: openArray[Frame], t: Frame): Frame =
   for i in 0..<CHANNELS:
     result[i] = sequence(seq[i], t[i])
+
+proc choose*[T](xs: openArray[T], t: float): T =
+  result = choose(xs, t, pool.data.choose[pool.index.choose])
+  pool.index.choose += 1
+
+proc choose*[T](xs: openArray[T], t: float, ps: openArray[float]): T =
+  result = choose(xs, t, ps, pool.data.choose[pool.index.choose])
+  pool.index.choose += 1
 
 proc phaser*(
   x: Frame,
@@ -254,3 +268,7 @@ proc zitarev*(
     level,
     pool.data.zitarev[pool.index.zitarev])
   pool.index.zitarev += 1
+
+# NB: using pooled ops inside callback will mess up cell assignment
+type WaveShaper = proc(x: float): float
+template ws*(body): WaveShaper = (proc(x {.inject.}: float): float = body)
