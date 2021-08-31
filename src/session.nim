7f848b12594538dc643296df44491dc25d891610
Ruslan Prakapchuk <fer.obbee@gmail.com>
2021-09-01 09:22:39 +1000

    Wed  1 Sep 2021 09:22:39 AEST

--- src/session.nim
+++ src/session.nim
@@ -29,8 +29,8 @@ proc process*(s: var State, cc: var Controls, n: var Notes,
       s.fft.synthesis_magnitudes[i] = (1.0-k)*s.fft.synthesis_magnitudes[i] + k*s.fft.analysis_magnitudes[i]
       s.fft.synthesis_frequencies[i] = (1.0-k)*s.fft.synthesis_frequencies[i] + k*s.fft.analysis_frequencies[i]
   z
-    .simple_saturator
     .mul(0.2)
+    .simple_saturator
 
 # A place for heavy init logic, like reading tables from the disk.
 # Beware access to the state is not guarded and may happen simultaneously with `process`.
