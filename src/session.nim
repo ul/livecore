c42f36e69bdbbe0f40cad5d7c0f9e6c229a35132
Ruslan Prakapchuk <fer.obbee@gmail.com>
2021-09-01 09:21:10 +1000

    Wed  1 Sep 2021 09:21:10 AEST

--- src/session.nim
+++ src/session.nim
@@ -4,22 +4,32 @@ import
   dsp/[frame, delays, effects, envelopes, events, filters, metro, modules,
         nanotidal, noise, osc, sampler, soundpipe, stereo, fft, patterns, fir,
         conv],
-  atomics, math, pool, control
+  atomics, math, pool, control, random
 
 type
   State* = object
     pool: Pool
-    p1: PSeq
+    phase1: float
+    fft: FFT1024
 
 proc process*(s: var State, cc: var Controls, n: var Notes,
     input: Frame): Frame {.nimcall, exportc, dynlib.} =
   s.pool.init
+  var x = 0.0
+  for i in 1..5:
+    x += 55.0.mul(i.to_float)
+    .add(white_noise().lpf(50.0).scale(-5.5, 5.5))
+    .blsaw
+    .mul((i.to_float/2.0).metro.impulse(0.1))
 
-  let freq = 2.dmetro.step(s.p1).mul(55.0)
-  let sig = 0.1 * freq.osc
-  let env = 1.metro.impulse(0.1)
+  let k = 0.25 * pink_noise()
 
-  sig.mul(env).simple_saturator
+  let z = process(x, s.fft):
+    for i in 0..<s.fft.bins:
+      s.fft.synthesis_magnitudes[i] = (1.0-k)*s.fft.synthesis_magnitudes[i] + k*s.fft.analysis_magnitudes[i]
+      s.fft.synthesis_frequencies[i] = (1.0-k)*s.fft.synthesis_frequencies[i] + k*s.fft.analysis_frequencies[i]
+  z
+    .mul(0.1)
 
 # A place for heavy init logic, like reading tables from the disk.
 # Beware access to the state is not guarded and may happen simultaneously with `process`.
@@ -29,8 +39,8 @@ proc load*(s: var State) {.nimcall, exportc, dynlib.} =
   # s.pool.addr.zero_mem(Pool.size_of)
   # s.addr.zero_mem(State.size_of)
   sp_create()
+  s.fft.init
 
-  [1.0, 2.5, 3.0, 3.5, 4.0].init(s.p1)
 
 # Clean up any garbage allocated outside of the State arena.
 # Beware access to the state is not guarded and may happen simultaneously with `process`.
