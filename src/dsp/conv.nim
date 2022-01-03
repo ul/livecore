ccb3cea06546e79e913570c1f24e1554af007fe7
Ruslan Prakapchuk <fer.obbee@gmail.com>
2022-01-03 15:50:29 +1100

    smooth out convolution over frames
    to reduce chance of overruns

--- src/dsp/conv.nim
+++ src/dsp/conv.nim
@@ -3,8 +3,8 @@
 import ffi/mufft/fft
 
 template defConv*(block_size, sub_filters: static[Natural]) =
-  ## `block_size` must be a power of two.
-  ## `sub_filters` is the number of blocks contributing into convolution as UPOLS subfilters.
+  ## `block_size` must be greater than `sub_filters` and be a power of two.
+  ## `sub_filters` is the number of blocks contributing into convolution as UPOLS subfilters, and must be a power of two.
   ##
   ## Generated type will be `Conv_{block_size}x{sub_filters}` with `init` and `process` "methods" available.
   ## Call `init` in session's `load` and `process` *once* in session's `process`
@@ -33,6 +33,8 @@ template defConv*(block_size, sub_filters: static[Natural]) =
       windows: array[2, TimeData]
       inputs_fdl: array[2*sub_filters, FrequencyData]
       output: Output
+      aot_cursor: int
+      fd: FrequencyData
 
     `Conv block_size x sub_filters`* {.inject.} = Conv
 
@@ -58,6 +60,22 @@ template defConv*(block_size, sub_filters: static[Natural]) =
     write_input_sample(s.inputs[0], x)
     write_input_sample(s.inputs[1], y)
 
+    # Compute partial sum for the next convolution ahead of time.
+    # This helps to smooth out computational load over frames.
+    # `sub_filters - 1` to skip the last element that will be pushed out
+    # at the next window shift.
+    const sub_filters_per_block = block_size div sub_filters
+    const aot_chunk = sub_filters
+    if s.aot_cursor < sub_filters_per_block * (sub_filters - 1):
+      let j = s.aot_cursor div sub_filters_per_block
+      let offset = aot_chunk * (s.aot_cursor mod sub_filters_per_block)
+      # One more element as fft_size = block_size + 1
+      if offset == 0:
+        s.fd[0] += s.inputs_fdl[2*j][0] * s.inputs_fdl[2*j+1][0]
+      for i in (offset + 1)..(offset + aot_chunk):
+        s.fd[i] += s.inputs_fdl[2*j][i] * s.inputs_fdl[2*j+1][i]
+      s.aot_cursor.inc
+
     if unlikely(s.output.cursor == 0):
       for n in 0..1:
         copy_mem(s.windows[n][0].addr, s.windows[n][block_size].addr,
@@ -71,16 +89,17 @@ template defConv*(block_size, sub_filters: static[Natural]) =
       mufft_execute_plan_1d(s.plan, s.inputs_fdl[0].addr, s.windows[0].addr)
       mufft_execute_plan_1d(s.plan, s.inputs_fdl[1].addr, s.windows[1].addr)
 
-      var fd: FrequencyData
       for i in 0..<fft_size:
-        for j in 0..<sub_filters:
-          fd[i] += s.inputs_fdl[2*j][i] * s.inputs_fdl[2*j+1][i]
+        s.fd[i] += s.inputs_fdl[0][i] * s.inputs_fdl[1][i]
 
       var td: TimeData
-      mufft_execute_plan_1d(s.iplan, td.addr, fd.addr)
+      mufft_execute_plan_1d(s.iplan, td.addr, s.fd.addr)
 
       for i in 0..<block_size:
         s.output.buffer[i] = norm * td[i + block_size]
+
+      s.aot_cursor = 0
+      zero_mem(s.fd.addr, FrequencyData.sizeof)
 
     read_output_sample(s.output)
 
