c7d8f12f4f325fa43c8088353427562d334f6861
Ruslan Prakapchuk <fer.obbee@gmail.com>
2022-09-11 08:08:45 +1000

    add shell.nix

---
+++ shell.nix
@@ -0,0 +1,12 @@
+with (import <nixpkgs> { });
+mkShell.override { stdenv = llvmPackages_14.stdenv; } {
+  buildInputs = [
+    nim
+    lld_14
+    fswatch
+    liblo
+    libsndfile
+    darwin.apple_sdk.frameworks.CoreAudio
+    darwin.apple_sdk.frameworks.AudioUnit
+  ];
+}
