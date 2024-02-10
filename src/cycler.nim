9ea4e66158b4f752d0ea3bc0ebfa0c0fcc497c0c
Ruslan Prakapchuk <fer.obbee@gmail.com>
2024-02-11 07:08:43 +1100

    limit polyphony

--- src/cycler.nim
+++ src/cycler.nim
@@ -1,78 +1,106 @@
 import
-  std/[options],
+  std/[algorithm, options, tables],
   dsp/[frame, osc],
   strudel/core/pattern
 
-type FastTimeSpan* = object
+type FastTimeSpan = object
   begin*: float
   `end`*: float
 
-type FastHap* = object
+type FastHap = object
   span*: FastTimeSpan
   value*: Controls
 
-# TODO Support multicycle patterns.
 type Cycler* = object
   cps: float # cycles per second
   phase: float # state for the saw oscillator that maintains the clock
   clock: float # current time in the cycle — not wall-clock time!
-  pending_events_cursor: int
-  pending_events: array[0x100, FastHap] # TODO warn on overflow
+  pending_events: seq[FastHap]
 
-converter to_fast_timespan*(span: TimeSpan): FastTimeSpan =
+converter to_fast_timespan(span: TimeSpan): FastTimeSpan =
   FastTimeSpan(
     begin: span.begin.to_float,
     `end`: span.`end`.to_float
   )
 
-converter to_fast_hap*(hap: Hap[Controls]): FastHap =
+converter to_fast_hap(hap: Hap[Controls]): FastHap =
   FastHap(
     span: hap.whole.get.to_fast_timespan,
     value: hap.value
   )
 
-proc tick*(s: var Cycler, cps: float = 0.5) =
-  s.cps = cps
-  s.clock = cps.saw(s.phase).uni
-
 proc cycle_duration*(s: var Cycler): float = s.cps.recip
 
 proc cycle_time*(s: var Cycler): float = s.clock * s.cycle_duration
 
-proc duration*(span: FastTimeSpan): float = span.`end` - span.begin
-proc duration*(e: FastHap): float = e.span.duration
-proc duration*[T](e: Hap[T], s: var Cycler): float = e.duration.to_float * s.cycle_duration
-proc duration*(e: FastHap, s: var Cycler): float = e.duration * s.cycle_duration
+proc duration(span: FastTimeSpan): float = span.`end` - span.begin
+proc duration(e: FastHap): float = e.span.duration
+proc duration[T](e: Hap[T], s: var Cycler): float = e.duration.to_float * s.cycle_duration
+proc duration(e: FastHap, s: var Cycler): float = e.duration * s.cycle_duration
 
-proc gate*(span: TimeSpan, s: var Cycler): float =
+proc gate(span: TimeSpan, s: var Cycler): float =
   if span.begin.to_float <= s.clock and s.clock < span.`end`.to_float:
     1.0
   else:
     0.0
 
-proc gate*[T](e: Hap[T], s: var Cycler): float = e.whole.get.gate(s)
+proc gate[T](e: Hap[T], s: var Cycler): float = e.whole.get.gate(s)
 
-proc gate*(span: FastTimeSpan, s: var Cycler): float =
+proc gate(span: FastTimeSpan, s: var Cycler): float =
   if span.begin <= s.clock and s.clock < span.`end`:
     1.0
   else:
     0.0
 
-proc gate*(e: FastHap, s: var Cycler): float = e.span.gate(s)
+proc gate(e: FastHap, s: var Cycler): float = e.span.gate(s)
 
-proc schedule*(s: var Cycler, p: Pattern[Controls]) =
-  s.pending_events_cursor = 0
+proc tick*(s: var Cycler, cps: float = 0.5) =
+  s.cps = cps
+  s.clock = cps.saw(s.phase).uni
+
+# TODO Support multicycle patterns.
+proc schedule*(s: var Cycler, p: Pattern[Controls], Δt: float, max_cps: float = 10.0, poly: int = 0x20) =
+  var voices = init_table[pointer, seq[FastHap]]()
   for hap in p.query(timespan(0, 1)):
-    let fast_hap = hap.to_fast_hap
-    s.pending_events[s.pending_events_cursor] = fast_hap
-    s.pending_events_cursor.inc
+    let k = hap.value.sound.get(nil)
+    if k.is_nil:
+      continue
+    if not voices.has_key(k):
+      voices[k] = @[]
+    voices[k].add(hap.to_fast_hap)
 
-# TODO Need a way to short-circuit instruments so they don't waste cycles.
-# Just returning silence on gate off doesn't work due to release and other effects.
-# Might do some kind of managed limited polyphony? E.g. a pool of 0x10 FIFO voices per instrument.
+  let clock = s.clock
+  # Events that begin during current block must be scheduled first.
+  let critical_zone = Δt * max_cps
+
+  func cw_distance(x: float): float =
+    let d = x - clock
+    if d < 0: d+1 else: d
+
+  func ccw_distance(x: float): float =
+    let d = clock - x
+    if d < 0: d+1 else: d
+
+  func by_begin(a, b: FastHap): int = cmp(a.span.begin, b.span.begin)
+  func by_end_distance(a, b: FastHap): int = cmp(a.span.`end`.ccw_distance, b.span.`end`.ccw_distance)
+
+  s.pending_events.set_len(0)
+  for voice in voices.values:
+    var selected, candidates: seq[FastHap]
+    for e in voice:
+      if e.span.begin.cw_distance <= critical_zone:
+        selected.add(e)
+      elif selected.len < poly:
+        candidates.add(e)
+    candidates.sort(by_end_distance)
+    candidates.set_len(poly - selected.len)
+    selected.add(candidates)
+    # Less disruption to the pool indexing.
+    selected.sort(by_begin)
+    s.pending_events.add(selected)
+
 proc process*(s: var Cycler, state: pointer): Frame =
-  for i in 0..(s.pending_events_cursor - 1):
-    var e = s.pending_events[i]
+  for e in s.pending_events.mitems:
     if e.value.sound.is_none or e.value.sound.get.is_nil:
       continue
     e.value.duration = e.duration(s)
--- src/session.nim
+++ src/session.nim
@@ -16,7 +16,7 @@ type
     looong: Delay300
 
 proc inst1(event: Controls, s: var State): Frame {.nimcall.} =
-  let x = event.note.get(silence).fm_osc(1/3, 1/5)
+  let x = event.note.get(silence).osc
   let a = 0.05.min(0.2*event.duration.max(1/64))
   let d = 2*a
   let sus = 0.5
@@ -26,7 +26,7 @@ proc inst1(event: Controls, s: var State): Frame {.nimcall.} =
     .mul(event.gain.get(1))
 
 proc inst2(event: Controls, s: var State): Frame {.nimcall.} =
-  let x = event.note.get(silence).fm_tri(1/3, 1/5)
+  let x = event.note.get(silence).bl_triangle
   let a = 0.05.min(0.2*event.duration.max(1/64))
   let d = 2*a
   let sus = 0.5
@@ -40,10 +40,11 @@ proc control*(s: var State, cc: var Controllers, n: var Notes, frame_count: int)
 
   let p = //[
     (//[!c4, e4, g4]).note.euclid(3, 8) >> gain([0.25, 0.1]),
+    (//[!c3, c4]).note.euclid(7, 8) >> gain([0.15, 0.2]),
     [!e4, g4, c3].note.euclid(2, 8) >> gain([0.2, 0.3])
   ] >> sound(//[! ^inst1, [nil, ^inst2]])
 
-  s.cycler.schedule(p)
+  s.cycler.schedule(p, frame_count.to_seconds, 1.0)
 
 proc audio*(s: var State, cc: var Controllers, n: var Notes, input: Frame): Frame {.nimcall, exportc, dynlib.} =
   ## This is called each frame to render the audio.
@@ -54,7 +55,7 @@ proc audio*(s: var State, cc: var Controllers, n: var Notes, input: Frame): Fram
   let choir = s.cycler.process(^s)
 
   choir
-    .long_fb(1.0, 0.5)
+    .long_ff(1.0, 0.5)
     .zita_rev(level=0)
     .simple_saturator
     .dc_block
