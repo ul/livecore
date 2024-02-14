0ba158a27748222ca29cf0278ba47a842a6a54b7
Ruslan Prakapchuk <fer.obbee@gmail.com>
2024-02-14 18:48:31 +1100

    make it stranger

--- src/session.nim
+++ src/session.nim
@@ -23,7 +23,7 @@ template prolly_echo(p: float, s: untyped) =
 
 proc inst0(e: Controls, s: var State): Frame =
   let x = e.note.get(silence).osc
-  let a = e.attack.get(1/64).min(0.2*e.duration).max(1/64)
+  let a = e.attack.get(1/64).tline(0.001).min(0.25*e.duration).max(1/64)
   let d = 2*a
   let sus = 0.5
   e.gate
@@ -32,20 +32,21 @@ proc inst0(e: Controls, s: var State): Frame =
     .mul(e.gain.get(1.0))
 
 proc inst1(e: Controls, s: var State): Frame =
-  let x = e.note.get(silence).bl_triangle * white_noise()
-  let a = e.attack.get(1/64).min(0.2*e.duration).max(1/64)
+  let x = e.note.get(silence).fm_osc(1/3, 1/3)
+  let a = e.attack.get(1/64).tline(0.001).min(0.5*e.duration).max(1/64)
   e.gate
     .impulse(a)
     .mul(x)
+    .mul(0.5)
     .mul(e.gain.get(1.0))
 
 proc control*(s: var State, m: Midi, frame_count: int) {.nimcall, exportc, dynlib.} =
   ## This is called each block before the audio is rendered.
   ## Many audio functions can be used here but keep in mind ×frame_count slowdown.
 
-  let a = (frame_count/33).osc.biscale(1/32, 1)
-  let b = (frame_count/23).osc.biscale(1/32, 1)
-  let c = (frame_count/13).osc.biscale(1/32, 1)
+  let a = (frame_count/33).tri.biscale(1/16, 2)
+  let b = (frame_count/23).tri.biscale(1/16, 2)
+  let c = (frame_count/13).tri.biscale(1/16, 2)
 
   let insts = [^inst0, ^inst1]
 
@@ -54,26 +55,30 @@ proc control*(s: var State, m: Midi, frame_count: int) {.nimcall, exportc, dynli
 
   let p1 = [
     note(c4) >> attack(a),
-    note([e4, e5]) >> attack(b),
+    !Controls(rest: true),
+    note([!e4, c5].stack) >> attack(b),
+    !Controls(rest: true),
     note(g4) >> attack(c),
   ]
-  let p = [p1.euclid(3, 8), p1.stack.euclid(3, 9)].stack >> sound(insts[s.fdt mod insts.len]) >> gain(0.8)
+  let p = p1.euclid(3, 8) >> sound(insts[s.fdt mod insts.len]) >> gain(0.2)
   s.cycler.schedule(p, frame_count.to_seconds, 1.0)
 
 proc audio*(s: var State, m: Midi, input: Frame): Frame {.nimcall, exportc, dynlib.} =
   ## This is called each frame to render the audio.
   s.pool.reset
 
-  let cycle_dur = (1/60).tri.biscale(2.0, 10.0) # seconds
+  let cycle_dur = (1/120).saw.biscale(10.0, 30.0) # seconds
   s.cycler.tick(cycle_dur.recip)
 
   let choir = process[var State](s.cycler, s).fadeout(s.fdt.to_float.trig_on_change, 0.01)
 
   choir
-    .ff(cycle_dur * 1.5, 0.5, s.looong)
-    .wp_korg35(c7, 0.95, 1.0)
+    .ff(( cycle_dur * 1.5 ) %% 1/4, 0.5, s.looong)
+    .ff(( cycle_dur * 2.25) %% 1/8, 0.5, s.looong) # deliberately reusing delay memory for moar strangeness
+    .wp_korg35(c6, 0.95, 1.0)
     .bqnotch_bw(315.0, 0.5)
     .bqnotch_bw(640.0, 1.0)
+    .bqhpf(100.0, 0.7071)
     .bqhpf(30.0, 0.7071)
     .zita_rev(level = 0)
     .simple_saturator
