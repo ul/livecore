7f0f2a0cc915dcce8e4f4decc98a784068b4d0d1
Ruslan Prakapchuk <fer.obbee@gmail.com>
2024-08-22 14:53:58 +1000

    adapt cicada church to the latest code

--- src/session.nim
+++ src/session.nim
@@ -1,119 +1,144 @@
 ## Where the creativity blossoms.
 
 import
-  std/[atomics, math, options, random],
-  dsp/[frame, delays, effects, envelopes, events, filters, metro, modules,
-        noise, osc, sampler, soundpipe, stereo, fft, fir, conv, notes],
-  strudel/core/pattern,
-  cycler, pool, control
+  dsp/[
+    frame, delays, effects, envelopes, events, filters, metro, modules, noise, osc,
+    sampler, soundpipe, stereo, fft, fir, conv,
+  ],
+  atomics,
+  math,
+  pool,
+  control
 
-defDelay(300)
+type State* = object
+  pool: Pool
+  convos: array[0x10, Conv8192x64]
 
-type
-  State* = object
-    pool: Pool
-    cycler: Cycler
-    looong: array[CHANNELS, Delay300]
-    atk: float
-    fdt: int
-    fft: array[CHANNELS, FFT4096]
+proc sum(xs: openArray[float]): float =
+  for x in xs:
+    result += x
 
-template prolly_echo(p: float, s: varargs[untyped]) =
-  if rand(1.0) < p:
-    echo s
+proc control*(
+    s: var State, m: var Midi, frame_count: int
+) {.nimcall, exportc, dynlib.} =
+  discard
 
-proc inst0(e: Controls, s: var State): Frame =
-  let x = e.note.get(silence).osc * pink_noise().cheb2
-  let a = e.attack.get(1/64).tline(0.001).min(0.25*e.duration).max(1/64)
-  let d = 2*a
-  let sus = 0.5
-  e.gate
-    .adsr(a, d, sus, d)
-    .mul(x)
-    .mul(e.gain.get(1.0))
-
-proc inst1(e: Controls, s: var State): Frame =
-  let x = e.note.get(silence).fm_osc(1/3, 1/3) * pink_noise().cheb2
-  let a = e.attack.get(1/64).tline(0.001).min(0.5*e.duration).max(1/64)
-  e.gate
-    .impulse(a)
-    .mul(x)
-    .mul(0.5)
-    .mul(e.gain.get(1.0))
-
-proc control*(s: var State, m: var Midi, frame_count: int) {.nimcall, exportc, dynlib.} =
-  ## This is called each block before the audio is rendered.
-  ## Many audio functions can be used here but keep in mind ×frame_count slowdown.
-
-  let a = (frame_count/33).tri.biscale(1/16, 2)
-  let b = (frame_count/23).tri.biscale(1/16, 2)
-  let c = (frame_count/13).tri.biscale(1/16, 2)
-
-  let insts = [^inst0, ^inst1]
-
-  if rand(1.0) < 0.005:
-    s.fdt += 1
-
-  let p1 = [
-    note(c4) >> attack(a),
-    !Controls(rest: true),
-    note([!e4, c5].stack) >> attack(b),
-    !Controls(rest: true),
-    note([!g4, e5].stack) >> attack(c),
-  ]
-  let p = p1.euclid(3, 8) >> sound(insts[s.fdt mod insts.len]) >> gain(0.2)
-  s.cycler.schedule(p, frame_count.to_seconds, 1.0)
-
-proc audio*(s: var State, m: var Midi, input: Frame): Frame {.nimcall, exportc, dynlib.} =
-  ## This is called each frame to render the audio.
+proc audio*(
+    s: var State, m: var Midi, input: Frame
+): Frame {.nimcall, exportc, dynlib.} =
   s.pool.reset
 
-  let cycle_dur = (1/120).saw.biscale(10.0, 30.0) # seconds
-  s.cycler.tick(cycle_dur.recip)
+  template voice(root, fmm, fmi, overtones, durs, cycle): float =
+    block:
+      template inst(freq: float): float =
+        freq
+        .fm_osc(fmm, fmi)
+        .add(freq.mul(2.0).fm_bltriangle(fmm, fmi).mul(0.05))
+        .add(freq.mul(3 / 2).fm_osc(fmm, fmi).mul(0.1))
+        .add(freq.mul(4 / 3).fm_osc(fmm, fmi).mul(0.1))
+        .add(freq.mul(1 / 2).fm_blsaw(fmm, fmi).mul(0.02))
 
-  let k = (m/0x1B)
+      var sig = 0.0
+      for i, a in overtones:
+        sig += a * inst((i + 1).float * root)
+      sig /= overtones.sum
 
-  let choir = process[var State](s.cycler, s)
-    .fadeout(s.fdt.to_float.trig_on_change, 0.01)
-    # .wp_korg35(c7, 0.95, 1.0)
-    # .bqhpf(100.0, 0.7071)
-    # .bqhpf(30.0, 0.7071)
-    .ff(( cycle_dur * 4.pow(m/0x13) ).quantize(1/32).tline(1/8), k, s.looong)
-    .ff(( cycle_dur * 2 * 4.pow(m/0x17)).quantize(1/16).tline(1/8), k, s.looong) # deliberately reusing delay memory for moar strangeness
+      let durc = durs.choose(cycle.dmetro)
+      let dm = durc.mul(4.0).dmetro.maytrig(0.5)
+      let dur = durc.sh(dm)
+      # let env = dm.adsr(0.1*dur, 0.1*dur, 0.9, 0.8*dur)
+      let env = dm.gaussian(0.5 * dur, (1 / 40).saw.mul(1 / 32).osc.biscale(0.05, 0.2))
+      sig.mul(env)
 
-  var signal: Frame = choir
+  # let root = (1 / 512).osc.add((1 / 300).osc).mul(0.5).biscale(12.0, 48.0).quantize(8.0)
+  let root = rline(30.0).scale(24.0, 48.0).quantize(4.0)
 
-  # for i in 0..CHANNELS-1:
-  #   signal[i] = choir[i].resynth(s.fft[i]):
-  #     for f in synthesis_frequencies.mitems:
-  #       f += f.mul(0.5).osc.mul(f/128)
-  #     var sum = 0.0
-  #     for a in synthesis_magnitudes:
-  #       sum += a
-  #     prolly_echo(0.001, "sum: ", sum)
-  #     if sum > 1000.0:
-  #       for a in synthesis_magnitudes.mitems:
-  #         a = a.sqrt
+  let voices = [
+    voice(
+      @(root + 24.0),
+      3 / 2,
+      1 / 3,
+      [
+        1.0,
+        (1 / 1).osc.biscale(0.1, 0.5),
+        (1 / 2).osc.biscale(0.1, 0.5),
+        (1 / 3).osc.biscale(0.1, 0.5),
+        (1 / 4).osc.biscale(0.1, 0.5),
+        (1 / 5).osc.biscale(0.1, 0.5),
+      ],
+      [1 / 4, 1, 1 / 4, 1, 1 / 4, 1],
+      2,
+    )
+    .mul(0.1),
+    voice(
+      @(root + 12.0),
+      4 / 3,
+      1 / 3,
+      [
+        1.0,
+        (1 / 6).osc.biscale(0.1, 0.5),
+        (1 / 7).osc.biscale(0.1, 0.5),
+        (1 / 8).osc.biscale(0.1, 0.5),
+        (1 / 9).osc.biscale(0.1, 0.5),
+        (1 / 10).osc.biscale(0.1, 0.5),
+      ],
+      [1 / 2, 1, 2, 2, 1, 1 / 2],
+      4,
+    )
+    .mul(0.2),
+    voice(
+      @root,
+      5 / 4,
+      1 / 3,
+      [
+        1.0,
+        (1 / 11).osc.biscale(0.1, 0.5),
+        (1 / 12).osc.biscale(0.1, 0.5),
+        (1 / 13).osc.biscale(0.1, 0.5),
+        (1 / 14).osc.biscale(0.1, 0.5),
+        (1 / 15).osc.biscale(0.1, 0.5),
+      ],
+      [4 / 1, 2, 1, 1, 2, 4],
+      8,
+    )
+    .mul(0.3),
+  ]
 
-  signal
-    .wp_korg35(c7, 0.95, 1.0)
-    .bqhpf(100.0, 0.7071)
-    .bqhpf(30.0, 0.7071)
-    .zita_rev(level = 0)
-    .simple_saturator
-    .dc_block
+  let k1 = @root
+    .tline(1.0).saw
+    .mul(whitenoise().scale(1.0, 3.0).dmetro.impulse(0.02))
+    .mul(0.001)
+
+  let k2 = @(root + 12.0)
+    .tline(1.0).tri
+    .mul(whitenoise().scale(0.30, 0.45).dmetro.impulse(0.05))
+    .mul(0.001)
+
+  silence
+  .add(voices[0])
+  .add(voices[1])
+  .add(voices[2])
+  .mul(0.25)
+  .process(pinknoise().decim(0.95).mul(0.05), s.convos[0])
+  .process(k1, s.convos[2])
+  # .process(k2, s.convos[1])
+  .dc_block
+  .bigverb(0.8, @(root + 48.0))
+  .wpkorg35(@(root + 36.0), 0.95, 0.5)
+  .bqhpf(30.0, 0.7071).saturator
 
 # A place for heavy init logic, like reading tables from the disk.
 # Beware access to the state is not guarded and may happen simultaneously with `control` or `audio`.
 proc load*(s: var State) {.nimcall, exportc, dynlib.} =
-  const MB = 1024^2
-  echo "State: ", int(State.size_of/MB), "MB / Pool: ", int(Pool.size_of/MB), "MB"
+  const MB = 1024 ^ 2
+  echo "State: ", int(State.size_of / MB), "MB / Pool: ", int(Pool.size_of / MB), "MB"
   # s.pool.addr.zero_mem(Pool.size_of)
   # s.addr.zero_mem(State.size_of)
   s.pool.init
   sp_create()
-  for i in 0..CHANNELS-1:
-    s.fft[i].init
+
+  for x in s.convos.mitems:
+    x.init
 
 # Clean up any garbage allocated outside of the State arena.
 # Beware access to the state is not guarded and may happen simultaneously with `control` or `audio`.
