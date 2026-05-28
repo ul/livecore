19807a4a7d10cacd889234ce89712e0e7ba581af
Ruslan Prakapchuk <fer.obbee@gmail.com>
2026-05-28 19:24:57 +1000

    remove deprecated darwin.apple_sdk

--- shell.nix
+++ shell.nix
@@ -1,11 +1,18 @@
 with (import <nixpkgs> { });
 mkShell {
-  buildInputs = [ nim lld fswatch libsndfile ] ++ (if stdenv.isDarwin then
-    (with darwin.apple_sdk.frameworks; [ AudioUnit CoreAudio ])
-  else
-    [ ]) ++ (if stdenv.isLinux then [ alsa-lib ] else [ ]);
-  LD_LIBRARY_PATH = if stdenv.isLinux then
-    lib.makeLibraryPath [ stdenv.cc.cc alsa-lib ]
-  else
-    "";
+  buildInputs = [
+    nim
+    lld
+    fswatch
+    libsndfile
+  ]
+  ++ (if stdenv.isLinux then [ alsa-lib ] else [ ]);
+  LD_LIBRARY_PATH =
+    if stdenv.isLinux then
+      lib.makeLibraryPath [
+        stdenv.cc.cc
+        alsa-lib
+      ]
+    else
+      "";
 }
